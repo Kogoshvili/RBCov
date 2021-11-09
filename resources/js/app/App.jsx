3aaf966621254ab4ca2365e4cfddf0fa8ea7d6b4
Andrian Kogoshvili <andriankogoshvili@gmail.com>
2021-11-10 01:31:09 +0400

    Init

---
+++ resources/js/app/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">TEST Component</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
